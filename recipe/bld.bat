fbc60441421d3c1c6177b14b1fb8d31d94c3e7bb
Ryan Volz <rvolz@mit.edu>
2018-05-10 14:03:36 -0400

    Update recipe for 3.8.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -10,17 +10,13 @@ cd build
 ::   DOXYGEN/SPHINX because we don't need docs in the conda package
 ::   TESTING because we don't intend to run the unit tests, just import test
 ::   GR_COMEDI needs comedi
-::   GR_FCD has libiconv linker error
 ::   GR_VIDEO_SDL needs sdl1 (conda-forge has sdl2)
 ::   GRC needs pygtk
 :: the following can be disabled to speed up the build (dependencies remain in
 :: meta.yaml):
-::   GR_ATSC
 ::   GR_CTRLPORT
 ::   GR_DTV
 ::   GR_FEC
-::   GR_NOAA
-::   GR_PAGER
 ::   GR_TRELLIS
 ::   GR_VIDEO_SDL
 ::   GR_VOCODER
@@ -36,7 +32,6 @@ cmake -G "NMake Makefiles JOM" ^
     -DENABLE_DOXYGEN=OFF ^
     -DENABLE_GNURADIO_RUNTIME=ON ^
     -DENABLE_GR_ANALOG=ON ^
-    -DENABLE_GR_ATSC=ON ^
     -DENABLE_GR_AUDIO=ON ^
     -DENABLE_GR_BLOCKS=ON ^
     -DENABLE_GR_COMEDI=OFF ^
@@ -44,12 +39,9 @@ cmake -G "NMake Makefiles JOM" ^
     -DENABLE_GR_CTRLPORT=ON ^
     -DENABLE_GR_DIGITAL=ON ^
     -DENABLE_GR_DTV=ON ^
-    -DENABLE_GR_FCD=OFF ^
     -DENABLE_GR_FEC=ON ^
     -DENABLE_GR_FFT=ON ^
     -DENABLE_GR_FILTER=ON ^
-    -DENABLE_GR_NOAA=ON ^
-    -DENABLE_GR_PAGER=ON ^
     -DENABLE_GR_QTGUI=ON ^
     -DENABLE_GR_TRELLIS=ON ^
     -DENABLE_GR_UHD=ON ^
@@ -57,13 +49,11 @@ cmake -G "NMake Makefiles JOM" ^
     -DENABLE_GR_VIDEO_SDL=OFF ^
     -DENABLE_GR_VOCODER=ON ^
     -DENABLE_GR_WAVELET=ON ^
-    -DENABLE_GR_WXGUI=ON ^
     -DENABLE_GR_ZEROMQ=ON ^
     -DENABLE_GRC=OFF ^
     -DENABLE_PYTHON=ON ^
     -DENABLE_SPHINX=OFF ^
     -DENABLE_TESTING=OFF ^
-    -DENABLE_VOLK=ON ^
     -DENABLE_INTERNAL_VOLK=OFF ^
     ..
 if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -16,17 +16,13 @@ cd build
 #   DOXYGEN/SPHINX because we don't need docs in the conda package
 #   TESTING because we don't intend to run the unit tests, just import test
 #   GR_COMEDI needs comedi
-#   GR_FCD has libiconv linker error
 #   GR_VIDEO_SDL needs sdl1 (conda-forge has sdl2)
 #   GRC needs pygtk
 # the following can be disabled to speed up the build (dependencies remain in
 # meta.yaml):
-#   GR_ATSC
 #   GR_CTRLPORT
 #   GR_DTV
 #   GR_FEC
-#   GR_NOAA
-#   GR_PAGER
 #   GR_TRELLIS
 #   GR_VOCODER
 #   GR_WAVELET
@@ -36,10 +32,10 @@ cmake -G "Ninja" \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DLIB_SUFFIX="" \
     -DPYTHON_EXECUTABLE=$PYTHON \
+    -DGR_PYTHON_DIR=`$PYTHON -c "import site; print(site.getsitepackages()[0])"` \
     -DENABLE_DOXYGEN=OFF \
     -DENABLE_GNURADIO_RUNTIME=ON \
     -DENABLE_GR_ANALOG=ON \
-    -DENABLE_GR_ATSC=ON \
     -DENABLE_GR_AUDIO=ON \
     -DENABLE_GR_BLOCKS=ON \
     -DENABLE_GR_COMEDI=OFF \
@@ -47,12 +43,9 @@ cmake -G "Ninja" \
     -DENABLE_GR_CTRLPORT=ON \
     -DENABLE_GR_DIGITAL=ON \
     -DENABLE_GR_DTV=ON \
-    -DENABLE_GR_FCD=OFF \
     -DENABLE_GR_FEC=ON \
     -DENABLE_GR_FFT=ON \
     -DENABLE_GR_FILTER=ON \
-    -DENABLE_GR_NOAA=ON \
-    -DENABLE_GR_PAGER=ON \
     -DENABLE_GR_QTGUI=ON \
     -DENABLE_GR_TRELLIS=ON \
     -DENABLE_GR_UHD=ON \
@@ -60,13 +53,11 @@ cmake -G "Ninja" \
     -DENABLE_GR_VIDEO_SDL=OFF \
     -DENABLE_GR_VOCODER=ON \
     -DENABLE_GR_WAVELET=ON \
-    -DENABLE_GR_WXGUI=ON \
     -DENABLE_GR_ZEROMQ=ON \
     -DENABLE_GRC=OFF \
     -DENABLE_PYTHON=ON \
     -DENABLE_SPHINX=OFF \
     -DENABLE_TESTING=OFF \
-    -DENABLE_VOLK=ON \
     -DENABLE_INTERNAL_VOLK=OFF \
     ..
 cmake --build . -- -j $((${CPU_COUNT}+2))

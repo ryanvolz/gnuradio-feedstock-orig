2e9e860632790388f35b0c6099a712d8257c059d
Ryan Volz <rvolz@mit.edu>
2018-05-10 14:14:48 -0400

    Rerender for prerelease branch, 3.8, and pkgw-forge repo for gtk3.

--- .circleci/build_steps.sh
+++ .circleci/build_steps.sh
@@ -12,7 +12,9 @@ cat >~/.condarc <<CONDARC
 
 channels:
  - conda-forge
+ - ryanvolz/label/prerelease
  - ryanvolz
+ - pkgw-forge
  - defaults
 
 conda-build:
@@ -28,15 +30,7 @@ conda clean --lock
 conda install --yes --quiet conda-forge-ci-setup=1 conda-build
 source run_conda_forge_build_setup
 
-
-# Install the yum requirements defined canonically in the
-# "recipe/yum_requirements.txt" file. After updating that file,
-# run "conda smithy rerender" and this line will be updated
-# automatically.
-/usr/bin/sudo -n yum install -y gtk2
-
-
 conda build /home/conda/recipe_root -m /home/conda/feedstock_root/.ci_support/${CONFIG}.yaml --quiet
-upload_or_check_non_existence /home/conda/recipe_root ryanvolz --channel=main -m /home/conda/feedstock_root/.ci_support/${CONFIG}.yaml
+upload_or_check_non_existence /home/conda/recipe_root ryanvolz --channel=prerelease -m /home/conda/feedstock_root/.ci_support/${CONFIG}.yaml
 
 touch "/home/conda/feedstock_root/build_artifacts/conda-forge-build-done-${CONFIG}"
